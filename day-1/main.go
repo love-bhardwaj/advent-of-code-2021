77bf80c2ca1d6402b9a82b23dbde9fd2eda50ed5
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-12-04 00:36:47 +0530

    complete both tasks of day 1

--- day-1/main.go
+++ day-1/main.go
@@ -18,6 +18,7 @@ func main(){
 	data := readData(file)
 
 	fmt.Println(checkIncrease(data))
+	fmt.Println(getIncreaseWindow(data))
 }
 
 func checkIncrease(data []int) int{
@@ -30,6 +31,33 @@ func checkIncrease(data []int) int{
 		}
 	}
 	return count
+}
+
+func getIncreaseWindow(data []int) int{
+	count := 0
+	for index := range data {
+		if index+4 > len(data) {
+			break
+		}
+		currWin := data[index:index+3]
+		nextWindow := data[index+1:index+4]
+		if sumSlice(nextWindow) > sumSlice(currWin) {
+			count++;
+		}
+	}
+	return count
+}
+
+func sumSlice(data []int) int {
+	sum := 0
+	if len(data) < 3{
+		return sum
+	} 
+
+	for _, value := range data {
+		sum += value
+	}
+	return sum
 }
 
 func readData(file *os.File) []int{
